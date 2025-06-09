15eb98bcf55602cf4b677a190a13440ce00a7b05
mumtaza <mumtazmohamed453@gmail.com>
2025-06-09 12:44:33 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
-const companyName = 'scuber';
-let mostprofitableNeighborhood = 'Chelsea'
-let companyCeo ='susan smith'
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
